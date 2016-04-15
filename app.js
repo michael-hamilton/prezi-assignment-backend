766a50a2fc67b7cfa25b5a82b35ec464030910ec
Michael Hamilton <michaelhamilton626@gmail.com>
2016-04-14 23:41:36 -0400

    Use port environment variable
    Allow port environment to be set.

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 var express = require('express'),
     app = express(),
-    controller = require('./controller');
-
-app.use(function (res, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Resource", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    next();
-});
+    controller = require('./controller'),
+    port = process.env.port || 3000;
 
 app.get('/prezis', controller.getPrezis);
 
-app.listen(3000);
+app.listen(port);
--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 var prezis = require(__dirname+'/prezis.json');
 
-//Send all the prezis in the JSON file
+//Send prezis back to client.  Sends prezis that match search query, or all prezis if no query is set.
 module.exports.getPrezis = function(req, res, next) {
     if(req.query.title) {
         var _matches = [];
