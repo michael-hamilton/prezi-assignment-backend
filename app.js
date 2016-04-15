8e4279cc549b97dfecdfd258b979667c9f90b081
Michael Hamilton <michaelhamilton626@gmail.com>
2016-04-15 00:09:56 -0400

    Add origin headers back in

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express'),
     app = express(),
     controller = require('./controller');
 
+
+app.use(function (res, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Resource", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+});
+
 app.set('port', (process.env.PORT || 5000));
 app.get('/prezis', controller.getPrezis);
 
